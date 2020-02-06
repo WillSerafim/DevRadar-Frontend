1d95eceddd01e8bd42832410940fb1c76caa61c7
Willlian Serafim <willian_50@ymail.com>
2020-02-06 19:55:50 -0300

    Correção import css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import api from './services/api';
 
 import './global.css';
-import './app.css';
+import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
